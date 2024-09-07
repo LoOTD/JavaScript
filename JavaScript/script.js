a4abfff36153791f3700dcbd893a2f607e9ee9d0
LoOTD <lotd86400@gmail.com>
2024-09-07 15:52:16 +0300

    Rework

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,12 +10,31 @@ const personalMoviesDB = {
     privat: false
 };
 
-const ask1 = prompt("Один из последних просмотренных фильмов?", ''),
-      ask2 = prompt("На сколько оцените просмотренный фильм по 10 школе?", ''),
-      ask3 = prompt("Один из последних просмотренных фильмов?", ''),
-      ask4 = prompt("На сколько оцените просмотренный фильм по 10 школе?", '');
+const repeat = 2;
 
-personalMoviesDB.movies[ask1] = ask2;
-personalMoviesDB.movies[ask3] = ask4;
+for(let i = 0; i < repeat; i++){
+    const ask1 = prompt("Один из последних просмотренных фильмов?", ''),
+        ask2 = prompt("На сколько оцените просмотренный фильм по 10 школе?", '');
+
+    if(ask1.length < 50 && ask1 != '' && ask2 != '' && ask1 != null && ask2 != null){
+        personalMoviesDB.movies[ask1] = ask2;
+    }else{
+        console.log('Error');
+        i--;
+    }
+}
+
+if(personalMoviesDB.count < 10){
+    alert('Просмотрено довольно мало фильмов');
+    console.log('Просмотрено довольно мало фильмов');
+}else if(personalMoviesDB.count >= 10 && personalMoviesDB.count < 30){
+    alert('Вы классический зритель');
+    console.log('Вы классический зритель');
+}else if(personalMoviesDB.count >= 30){
+    alert('Вы киноман');
+    console.log('Вы киноман')
+}else{
+    alert('Произошла ошибка')
+}
 
 console.log(personalMoviesDB);
